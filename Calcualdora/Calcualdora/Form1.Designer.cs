320f2857d1476fba0f3ecd37753046a761f1f25b
JoseRamirezhz <47959020+JoseRamirezhz@users.noreply.github.com>
2019-03-31 16:50:16 -0600

    Add files via upload

---
+++ Calcualdora/Calcualdora/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Calcualdora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRBSelec = new System.Windows.Forms.TextBox();
+            this.lblOpcion = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Location = new System.Drawing.Point(154, 83);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Location = new System.Drawing.Point(154, 119);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton3.Location = new System.Drawing.Point(154, 157);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton4.Location = new System.Drawing.Point(154, 189);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(259, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Resultado:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(370, 97);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(122, 20);
+            this.txtNum1.TabIndex = 7;
+            this.txtNum1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(370, 171);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(122, 20);
+            this.txtNum2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(366, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Numero 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(366, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Numero 2";
+            // 
+            // txtRBSelec
+            // 
+            this.txtRBSelec.Location = new System.Drawing.Point(316, 332);
+            this.txtRBSelec.Name = "txtRBSelec";
+            this.txtRBSelec.Size = new System.Drawing.Size(122, 20);
+            this.txtRBSelec.TabIndex = 11;
+            // 
+            // lblOpcion
+            // 
+            this.lblOpcion.AutoSize = true;
+            this.lblOpcion.BackColor = System.Drawing.Color.Transparent;
+            this.lblOpcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOpcion.Location = new System.Drawing.Point(82, 373);
+            this.lblOpcion.Name = "lblOpcion";
+            this.lblOpcion.Size = new System.Drawing.Size(0, 24);
+            this.lblOpcion.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(97, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Seleccione una Opcion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Calcualdora.Properties.Resources.vector_illustration_of_abstract_blue_wave;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(638, 464);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblOpcion);
+            this.Controls.Add(this.txtRBSelec);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRBSelec;
+        private System.Windows.Forms.Label lblOpcion;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Calcualdora/Calcualdora/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcualdora
+{
+    public partial class Form1 : Form
+    {
+        double n1, n2, r;
+        public Form1()
+        {
+            InitializeComponent();
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (radioButton1.Checked == true)
+            {
+                
+                string RBselec;
+                n1 = double.Parse(txtNum1.Text);
+                n2 = double.Parse(txtNum2.Text);
+                r = n1 + n2;
+                RBselec = "Usted ha seleccionado la opcion de Suma";
+
+                txtRBSelec.Text = r.ToString();
+                lblOpcion.Text = RBselec.ToString();
+            }
+            else
+            {
+                if (radioButton2.Checked == true)
+                {
+                    string RBselec;
+                    n1 = double.Parse(txtNum1.Text);
+                    n2 = double.Parse(txtNum2.Text);
+                    r = n1 - n2;
+                    RBselec = "Usted ha seleccionado la opcion de Resta";
+
+                    txtRBSelec.Text = r.ToString();
+                    lblOpcion.Text = RBselec.ToString();
+                }
+                else
+                {
+                    if (radioButton3.Checked == true)
+                    {
+                        string RBselec;
+                        n1 = double.Parse(txtNum1.Text);
+                        n2 = double.Parse(txtNum2.Text);
+                        r = n1 * n2;
+                        RBselec = "Usted ha seleccionado la opcion de Multiplicacion";
+
+                        txtRBSelec.Text = r.ToString();
+                        lblOpcion.Text = RBselec.ToString();
+                    }
+                    else
+                    {
+                        if (radioButton4.Checked == true)
+                        {
+                            string RBselec;
+                            n1 = double.Parse(txtNum1.Text);
+                            n2 = double.Parse(txtNum2.Text);
+                            r = n1 / n2;
+                            RBselec = "Usted ha seleccionado la opcion de Division";
+
+                            txtRBSelec.Text = r.ToString();
+                            lblOpcion.Text = RBselec.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
